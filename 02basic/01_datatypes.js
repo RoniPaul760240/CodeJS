3dda2d5e75923c325df503e90e955e2772eb6d1e
RoniPaul760240 <156935394+RoniPaul760240@users.noreply.github.com>
2024-01-20 10:03:48 +0000

    datatype

---
+++ 02basic/01_datatypes.js
@@ -0,0 +1,9 @@
+"use strict"; //treat all js code as newer version
+//number => 2 to power 59
+//bigint =
+//string = " "
+//boolean =>true/false
+//null => standalone value
+//undefined => no value
+//symbol => unique
+// null is object // interview question
