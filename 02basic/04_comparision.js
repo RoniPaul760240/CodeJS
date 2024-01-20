820e47d86d753cf5ac62ba8770c9245c668a2015
RoniPaul760240 <156935394+RoniPaul760240@users.noreply.github.com>
2024-01-20 14:40:06 +0000

    Comparison

---
+++ 02basic/04_comparision.js
@@ -0,0 +1,18 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);\
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null>0);
+console.log(null,null,null>=0);
+console.log(null,null,null<0);
+console.log(null==0);
+console.log(null!=0);
+console.log("2" === 2);
+
+
