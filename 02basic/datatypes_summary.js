f1f5c8b1be040e01bde2ae9b55133394618f2999
RoniPaul760240 <156935394+RoniPaul760240@users.noreply.github.com>
2024-01-20 15:28:05 +0000

    Datatypes

---
+++ 02basic/datatypes_summary.js
@@ -0,0 +1,34 @@
+//javascript is a statically or dynamically typed?
+//JavaScript is a dynamically typed language. This means that the data type of a variable is not declared and can change during the program's execution. For example, you can assign a string to a variable, and then later assign a number to the same variable. The variable will now be of type number.
+//Static typing, on the other hand, is a system where the data type of a variable is declared and cannot change. This can help to prevent errors, as the compiler will check to make sure that the data types of variables are compatible.
+
+
+//So JavaScript is a dynamically typed language.(interview question)
+
+
+// Data Types In js ==> (1) Primitive Datatype, (2) Non Primitive Datatype
+// 7 types primitive datatype :-> (1) String, (2) Number, (3) Bollean, (4) Null, (5) Undifind, (6) Symbol, (7) BigInt..
+
+// String Datatype:-> Exampale
+const name = "Hello!";
+// Number Datatype:-> Exampale
+const num = 213;
+//Boolean datatype:-> Exampale
+const isLoggedIn = true/false;
+//Null Datatype:-> Exampale
+const outsideTemp = null;
+//Undifind datatype:-> Exampale
+let emailId;
+// Symbol Datatype:-> Exampale
+const Id = Symbol('123');
+const anotherId = Symbol('2135');
+console.log(Id === anotherId);
+//BigInt Datatype:-> Exampale
+const bigNumber = 1223557452484;
+
+
+
+
+// Referance (Non Primitive Datatype)
+//(1) Array, (2) Object (3) Function ........
+
