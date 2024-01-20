15aa6713c378bac8a0347c3687b96b8c8b2def8c
RoniPaul760240 <156935394+RoniPaul760240@users.noreply.github.com>
2024-01-20 11:28:05 +0000

    conversionoperatiopn

---
+++ 02basic/03_conversionoperation.js
@@ -0,0 +1,24 @@
+// let num = "Ayan"
+// console.log(typeof num);
+// console.log(typeof num);
+
+// let valueInNumber = Number(num);
+// console.log(valueInNumber);
+
+// "33" = print number
+// "33abd" = print as a NAN(Not a Number)
+// true=>1; false=>0
+//
+
+// let isLoggedIn = ""
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+// //1 =>"true", 0 =>"false"
+//"" =>"false"
+//"abs" =>"true"
+
+let someNumber = 63
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
