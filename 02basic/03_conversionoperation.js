24558249701b527c732b40bfe6e9d10f35626a74
RoniPaul760240 <156935394+RoniPaul760240@users.noreply.github.com>
2024-01-20 12:21:21 +0000

    Operations

--- 02basic/03_conversionoperation.js
+++ 02basic/03_conversionoperation.js
@@ -18,7 +18,44 @@
 //"" =>"false"
 //"abs" =>"true"
 
-let someNumber = 63
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 63
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ************************* Operations ********************************
+
+// console.log(2+8);
+// console.log(2*8);
+// console.log(2/8);
+// console.log(2-8);
+// console.log(2**3);
+// console.log(2%8);
+
+
+
+let value = 5;
+let posValue = +value;
+let negValue = -value;
+// console.log(posValue);
+// console.log(negValue);
+
+
+let str1 = "hello";
+let str2 = " Ayan";
+let str3 = str1 + str2;
+// console.log(str3);
+
+let gamecounter = 100;
+gamecounter++;
+console.log(gamecounter);
+
+
+// Description for prefix and postfix
+
+// The ++ operator is overloaded for two types of operands: number and BigInt. It first coerces the operand to a numeric value and tests the type of it. It performs BigInt increment if the operand becomes a BigInt; otherwise, it performs number increment.
+
+// If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.
+
+// If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
