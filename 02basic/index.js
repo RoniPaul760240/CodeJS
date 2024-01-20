43335dfd506693042a48f628402ba8cd7928f17e
RoniPaul760240 <156935394+RoniPaul760240@users.noreply.github.com>
2024-01-20 09:34:31 +0000

    index

--- 02basic/index.js
+++ 02basic/index.js
@@ -1,1 +1,9 @@
-console.log("Ayan")
+const accountId =14456
+let accountEmail = "abc@gmail.com"
+var accountPassword = 258630
+accountCity = "jaipur"
+
+console.table([accountEmail,accountId,accountPassword,accountCity]);
+
+
+
