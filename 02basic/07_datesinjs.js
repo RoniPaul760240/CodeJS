de888ab174d81095c8443b2c07b5b2052a9143fe
RoniPaul760240 <156935394+RoniPaul760240@users.noreply.github.com>
2024-01-31 15:30:12 +0000

    save

---
+++ 02basic/07_datesinjs.js
@@ -0,0 +1,16 @@
+// let myDate = new Date ()
+// console.log(myDate. toString ());
+// console.log(typeof  myDate);
+
+let myCreateDate = new Date ("2023-01-28")
+// console.log(myCreateDate. toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreateDate. getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date ()
+console.log(newDate.getDay ());
+console.log(newDate. getMonth ()+1); 
+console.log(newDate. getFullYear ());
