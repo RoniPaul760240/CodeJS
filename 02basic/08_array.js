8c6d34f69e4a1842fa106e3e854be7cc7f2d8fe8
RoniPaul760240 <156935394+RoniPaul760240@users.noreply.github.com>
2024-02-03 15:58:45 +0000

    save

--- 02basic/08_array.js
+++ 02basic/08_array.js
@@ -47,4 +47,10 @@ const all_new_heros = [...marvel_heros, ...dc_heros]
 
 console.log (Array.isArray("AyanPaul"))
 console.log(Array.from("AyanPaul"))
-console.log(Array.from({name: "AyanPAul"}))
+// console.log(Array.from({name: "AyanPAul"})) // interesting question interview 
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3));
