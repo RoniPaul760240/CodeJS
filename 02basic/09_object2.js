91a1d83b53023908e11788f78d9986237fd5f677
RoniPaul760240 <156935394+RoniPaul760240@users.noreply.github.com>
2024-02-13 20:10:33 +0000

    save

--- 02basic/09_object2.js
+++ 02basic/09_object2.js
@@ -5,4 +5,26 @@ tinderUser.email = "ayanpal20@hotmail.com"
 tinderUser.gender = "male"
 tinderUser.loggedin = "true"
 
-console.table(tinderUser);
+// console.table(tinderUser);
+
+const regularUser = {
+    email: "palayan82@gmail.com",
+    fullname: {
+        userfullname:  {
+            fristname: "Ayan",
+            lastname: "Paul"
+    }
+   
+    }
+}
+// console.log(regularUser.fullname);
+
+const obj1 = {1:"a", 2:"b"}
+
+const obj2 = {3:"a", 4:"c"}
+
+// const obj3 = {obj1, obj2}
+
+const obj3 = Object.assign ( {}, obj1, obj2)
+
+console.log(obj3);
