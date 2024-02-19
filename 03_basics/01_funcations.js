a438a74bfee076b3c8a3deb5a8fc4f3bd21eb90d
RoniPaul760240 <156935394+RoniPaul760240@users.noreply.github.com>
2024-02-19 19:44:57 +0000

    save

--- 03_basics/01_funcations.js
+++ 03_basics/01_funcations.js
@@ -1,63 +1,21 @@
+function sayMyName(){
 
-
-function SayMyName() {
     console.log("A");
     console.log("Y");
     console.log("A");
     console.log("N");
 }
 
-// SayMyName()
+// sayMyName()
 
-// function addTwoNumbers (number1, number2) {   //(when you make a defination of function and input numbers , strings they call are peramiters)
-
-//     console.log(number1+number2);   // (when you call the funcations and inputs numbers and strings they are call arguments)
+// function addTwoNumbers(num1, num2){
+//     console.log(num1 + num2);
 // }
 
-
-function addTwoNumbers (number1, number2) {
-    // let result = number1 + number2
-    // return result
-
-    return number1 + number2
-}
-const result = addTwoNumbers(99, 22)
-// console.log("Result: ", result);
-
-function loginUserMassage (username = "shyam"){
-    if(!username){
-        console.log("please enter a user name");
-        return
-    }
-    return`${username} userjustoggedin`
-}
-// console.log(loginUserMassage("Ayan"))
-// console.log(loginUserMassage("ayan"))
-
-function calculatecartprice (val1,val2,...num1) {
-    return num1
-}
-// console.log(calculatecartprice(200,300,400,5000));
-
-const user = {
-    productname: "mars serum",
-    prices: 499
+function addTwoNumbers(num1, num2){
+    let result = num1 + num2
+    return result
 }
 
-function handleobject (anyobject){
-    console.log(`product name is ${anyobject.productname} and price is ${anyobject.price}`);
-}
-// handleobject(user)
-handleobject({
-    productname: "ponds serum",
-    price: 599
-})
-
-const myNewArray = [200,899,255,4174,1258]
-
-function returnSecondvalue (getArray){
-    return getArray[6]
-}
-// console.log(returnSecondvalue(myNewArray));
-console.log(returnSecondvalue([200,565,789,417,5856,1254,5858]));
-
+const result = addTwoNumbers(5,6)
+console.log("Result: ", result);
