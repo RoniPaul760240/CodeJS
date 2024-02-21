f13991a9f59556146af7247026a24f0dcb483eb6
RoniPaul760240 <156935394+RoniPaul760240@users.noreply.github.com>
2024-02-21 20:26:54 +0000

    save

--- 03_basics/01_funcations.js
+++ 03_basics/01_funcations.js
@@ -1,32 +1,64 @@
-function sayMyName(){
+// function sayMyName(){
 
+//     console.log("A");
+//     console.log("Y");
+//     console.log("A");
+//     console.log("N");
+// }
+
+// // sayMyName()
+
+// // function addTwoNumbers(num1, num2){
+// //     console.log(num1 + num2);
+// // }
+
+// function addTwoNumbers(num1, num2){
+//     let result = num1 + num2
+//     return result
+// }
+
+// const result = addTwoNumbers(5,6)
+// // console.log("Result: ", result);
+
+// function logInUser(userName = "Ram"){
+//     if(!userName){
+//         console.log("Please enter a user name");
+//         return
+//     }
+//     return `${userName} just log in` 
+// }
+// // const myUser = logInUser("Ayan")
+// console.log(logInUser("Ayan"));
+// // console.log(logInUser());
+
+function myName(){
     console.log("A");
-    console.log("Y");
+    console.log("y");
     console.log("A");
     console.log("N");
 }
+// myName()
 
-// sayMyName()
-
-// function addTwoNumbers(num1, num2){
+// function addTwoNumbers(num1,num2){         //when we are make the function defination and iputs the value (number and string) they call peramiters
 //     console.log(num1 + num2);
+
 // }
 
-function addTwoNumbers(num1, num2){
-    let result = num1 + num2
-    return result
+function addTwoNumbers(num1,num2){ 
+//    let result = num1+num2
+ return num1 + num2
 }
+const result = addTwoNumbers(3 , 5)   // arguments
 
-const result = addTwoNumbers(5,6)
-// console.log("Result: ", result);
+// console.log("Result :" , result);
 
-function logInUser(userName = "Ram"){
-    if(!userName){
+
+function logInUser(username){
+    if(username === undefined){
         console.log("Please enter a user name");
         return
     }
-    return `${userName} just log in` 
+    return `${username} just log in`
 }
-// const myUser = logInUser("Ayan")
-console.log(logInUser("Ayan"));
-// console.log(logInUser());
+console.log(logInUser());
+
