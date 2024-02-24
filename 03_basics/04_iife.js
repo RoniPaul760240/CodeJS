d766d9ab7c53c946e8972802eb32e57b0d41bc02
RoniPaul760240 <156935394+RoniPaul760240@users.noreply.github.com>
2024-02-24 19:00:50 +0000

    save

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,1 +1,11 @@
 // Immediately Invoked Function Expressions (IIFE)
+
+// *** interview Global scope k polution se problem hoti hein kabhi khabhi to us global scope jo veriable hein and jo bhi declearation hein us polution ko remove korne k liya IIFE use kiya 
+
+(function chai(){              // This is name IIFE  ***interview
+    console.log("DB CONNECTED");
+})();
+
+( (name) => {             // Un name IIFE ***interview 
+    console.log(`DB SUCEESFULLY CONECTED ${name}`);
+})('ayan');
