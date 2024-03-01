40f775a84ae584ba67eb66603bc3e8f6da6109e7
RoniPaul760240 <156935394+RoniPaul760240@users.noreply.github.com>
2024-03-01 18:04:33 +0000

    save

---
+++ 04_control_flow/one.js
@@ -0,0 +1,51 @@
+// IF
+//<, >, <=, >=, ==, !=, ===
+
+// if(2 != 3){
+//     // console.log("executed");
+// }
+
+// const temperature = 20
+// if(temperature === 20){
+//     console.log("temperature is less then 50");
+// }
+// else{
+//     console.log("temperature is high");
+// }
+
+const score = 200
+if(score > 100){
+   let power = "fly"
+   console.log(`user power : ${power}`);
+}
+
+// console.log(`user power : ${power}`);
+
+// const amount = 2000
+
+// if(amount < 2000){
+//     console.log("less then 500");
+// }
+// else if ( amount < 600){
+//     console.log("less then 200");
+
+// }
+// else if ( amount < 900){
+//     console.log("less then 800");
+
+// }
+// else if ( amount < 900){
+//     console.log("less then 200");
+
+// }
+// else if ( amount < 600){
+//     console.log("less then 200");
+
+// }
+// else{
+//     console.log("less then 1200");
+
+// }
+
+
+
